19990f916f480bf809f6c60515233cb5cd5faa6c
Umayangi Ekanayake <umayangi1999@gmail.com>
2023-09-29 11:40:01 +0530

    fixed the security misconfiguration vulnerability in AppBooking.js

--- routes/api/AppBooking.js
+++ routes/api/AppBooking.js
@@ -1,31 +1,59 @@
 const express = require("express");
 const AppBooking = require("../../models/AppBooking");
+const { body, validationResult } = require("express-validator");
 
 const router = express.Router();
 
-//save appointment
-router.post("/appbooking/save", (req, res) => {
-  let newPost = new AppBooking(req.body);
+// Validation middleware
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
 
-  newPost.save((err) => {
-    if (err) {
-      return res.status(400).json({
-        error: err,
+// Save appointment
+router.post(
+  "/appbooking/save",
+  [
+    //validation
+    body("title").notEmpty().trim().isLength({ max: 255 }),
+    body("pname").notEmpty().trim().isLength({ max: 255 }),
+    body("mobile").notEmpty().trim().isMobilePhone(),
+    body("date").isISO8601(),
+    body("email").notEmpty().trim().isEmail(),
+    body("nicpass")
+      .notEmpty()
+      .trim()
+      .isLength({ min: 12, max: 12 })
+      .isNumeric(),
+    body("area").notEmpty().trim().isLength({ max: 255 }),
+  ],
+  validate,
+  (req, res) => {
+    const newPost = new AppBooking(req.body);
+
+    newPost.save((err) => {
+      if (err) {
+        return res.status(500).json({
+          error: "Internal Server Error. Failed to save appointment.",
+        });
+      }
+
+      return res.status(200).json({
+        success: "Appointment booked successfully.",
       });
-    }
-
-    return res.status(200).json({
-      success: "Appointment Booked Successfully",
     });
-  });
-});
+  }
+);
 
-//get appointments
+// Get appointments
 router.get("/appbooking", (req, res) => {
   AppBooking.find().exec((err, posts) => {
     if (err) {
-      return res.status(400).json({
-        error: err,
+      return res.status(500).json({
+        error: "Internal Server Error. Failed to fetch appointments.",
       });
     }
     return res.status(200).json({
@@ -35,13 +63,21 @@ router.get("/appbooking", (req, res) => {
   });
 });
 
-//get a specific appointment
+// Get a specific appointment
 router.get("/appbooking/:id", (req, res) => {
-  let postId = req.params.id;
+  const postId = req.params.id;
 
   AppBooking.findById(postId, (err, post) => {
     if (err) {
-      return res.status(400).json({ success: false, err });
+      return res.status(500).json({
+        error: "Internal Server Error. Failed to fetch appointment.",
+      });
+    }
+
+    if (!post) {
+      return res.status(404).json({
+        error: "Appointment not found.",
+      });
     }
 
     return res.status(200).json({
@@ -51,34 +87,68 @@ router.get("/appbooking/:id", (req, res) => {
   });
 });
 
-//update appointment
-router.put("/appbooking/update/:id", (req, res) => {
-  AppBooking.findByIdAndUpdate(
-    req.params.id,
-    {
-      $set: req.body,
-    },
-    (err, post) => {
-      if (err) {
-        return res.status(400).json({ error: err });
+// Update appointment
+router.put(
+  "/appbooking/update/:id",
+  [
+    //validation
+    body("title").notEmpty().trim().isLength({ max: 255 }),
+    body("pname").notEmpty().trim().isLength({ max: 255 }),
+    body("mobile").notEmpty().trim().isMobilePhone(),
+    body("date").isISO8601(),
+    body("email").notEmpty().trim().isEmail(),
+    body("nicpass")
+      .notEmpty()
+      .trim()
+      .isLength({ min: 12, max: 12 })
+      .isNumeric(),
+    body("area").notEmpty().trim().isLength({ max: 255 }),
+  ],
+  validate,
+  (req, res) => {
+    AppBooking.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      (err, post) => {
+        if (err) {
+          return res.status(500).json({
+            error: "Internal Server Error. Failed to update appointment.",
+          });
+        }
+
+        if (!post) {
+          return res.status(404).json({
+            error: "Appointment not found.",
+          });
+        }
+
+        return res.status(200).json({
+          success: "Appointment updated successfully.",
+        });
       }
-      return res.status(200).json({
-        success: "Updated Succesfully",
+    );
+  }
+);
+
+// Delete appointment
+router.delete("/appbooking/delete/:id", (req, res) => {
+  AppBooking.findByIdAndRemove(req.params.id, (err, deletedpost) => {
+    if (err) {
+      return res.status(500).json({
+        error: "Internal Server Error. Failed to delete appointment.",
       });
     }
-  );
-});
 
-//delete notes
-router.delete("/appbooking/delete/:id", (req, res) => {
-  AppBooking.findByIdAndRemove(req.params.id).exec((err, deletedpost) => {
-    if (err)
-      return res.status(400).json({
-        message: "Delete Unsuccesfull",
-        err,
+    if (!deletedpost) {
+      return res.status(404).json({
+        error: "Appointment not found.",
       });
-    return res.json({
-      message: "Delete Successfull",
+    }
+
+    return res.status(200).json({
+      success: "Appointment deleted successfully.",
       deletedpost,
     });
   });
